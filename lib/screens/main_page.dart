61e5dd29ba0599a273a2ef6f6c9062d85f4a087a
LAPTOP-FGFQK50O\LENOVO <ninkaishou27@gmail.com>
2023-04-18 03:39:46 +0700

    main page done

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,6 +14,8 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  bool isLoading = true;
+
   List<MemeModel>? listMemes;
 
   @override
@@ -24,7 +27,9 @@ class _MainPageState extends State<MainPage> {
 
   getData() async {
     listMemes = await ApiData.getAllMemes();
-    
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -40,33 +45,58 @@ class _MainPageState extends State<MainPage> {
               fontSize: 18, fontWeight: FontWeight.w500, color: Colors.black),
         ),
       ),
-      body: ListView(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: Text(
-              'Choose your memes',
-              style: GoogleFonts.poppins(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : RefreshIndicator(
+              onRefresh: () async {
+                setState(() {
+                  isLoading = true;
+                });
+                List<MemeModel>? newList;
+                newList = await ApiData.getAllMemes();
+                setState(() {
+                  listMemes = newList;
+                  isLoading = false;
+                });
+              },
+              child: ListView(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Text(
+                      'Choose your memes',
+                      style: GoogleFonts.poppins(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: listMemes?.length,
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3),
+                      itemBuilder: (context, index) {
+                        return CachedNetworkImage(
+                          imageUrl: listMemes?[index].url ?? '',
+                          placeholder: (context, url) =>
+                              CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              Icon(Icons.error),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          GridView.builder(
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-            itemBuilder: (context, index) {
-              return Image.network(
-                listMemes?[index].url ?? '',
-                scale: 10,
-              );
-            },
-          )
-        ],
-      ),
     );
   }
 }
